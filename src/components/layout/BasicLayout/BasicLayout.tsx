e06957278314883ab91c23be327e58bae35b639b
rabbitblood <qz154772610@gmail.com>
2024-05-04 15:54:55 -0700

    scroll to top

--- src/components/layout/BasicLayout/BasicLayout.tsx
+++ src/components/layout/BasicLayout/BasicLayout.tsx
@@ -1,8 +1,15 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { useLocation } from "react-router-dom";
 
 export default function BasicLayout({ children }: PropsWithChildren) {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Header />
