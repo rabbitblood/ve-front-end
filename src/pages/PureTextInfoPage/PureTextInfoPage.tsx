0afe240c7b839f90a3a9e18a66971b45f6c14efc
rabbitblood <qz154772610@gmail.com>
2024-05-14 14:29:02 -0700

    builder io custom page

--- src/pages/PureTextInfoPage/PureTextInfoPage.tsx
+++ src/pages/PureTextInfoPage/PureTextInfoPage.tsx
@@ -1,18 +1,31 @@
 import BasicLayout from "@/components/layout/BasicLayout/BasicLayout";
+import builder, { BuilderComponent } from "@builder.io/react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function PureTextInfoPage() {
   const { pagename } = useParams<{ pagename: string }>();
 
+  const [builderContentJson, setBuilderContentJson] = useState(null);
+
+  useEffect(() => {
+    builder
+      .get("custom-page", {
+        url: "https://vestudio.ca/info/return-and-refund",
+      })
+      .promise()
+      .then((data) => {
+        console.log(data);
+        setBuilderContentJson(data);
+      });
+  }, []);
+
   return (
-    <BasicLayout>
-      <div>
-        <h2>{pagename}</h2>
-        <h1>PureTextInfoPage</h1>
-        <pre>{`"text
-         in this 
-         page"`}</pre>
-      </div>
-    </BasicLayout>
+    builderContentJson && (
+      <BasicLayout>
+        {pagename}
+        <BuilderComponent model="custom-page" content={builderContentJson} />
+      </BasicLayout>
+    )
   );
 }
