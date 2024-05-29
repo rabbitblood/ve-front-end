cc6950ec26559ec710e619af770f4c926e95c52e
rabbitblood <qz154772610@gmail.com>
2024-05-29 00:25:02 -0700

    product intro page animation

--- src/components/organisms/IntroSection/IntroSection.tsx
+++ src/components/organisms/IntroSection/IntroSection.tsx
@@ -11,6 +11,7 @@ import "./IntroSection.css";
 import { FormButton } from "@/components/atoms/FormButton/FormButton";
 import { Link } from "react-router-dom";
 import { StringToUppercasedFirstLetterParagraphElement } from "@/lib/util/paragraphUtil";
+import { motion } from "framer-motion";
 
 interface IntroSectionProps extends HtmlHTMLAttributes<HTMLElement> {
   title: string;
@@ -23,46 +24,104 @@ interface IntroSectionProps extends HtmlHTMLAttributes<HTMLElement> {
 
 const IntroSection = forwardRef(
   (props: IntroSectionProps, ref: LegacyRef<HorizontalMoveImageViewerRef>) => {
+    //framer animations
+    const displaySectionAnimationVariant = {
+      hidden: {
+        opacity: 0,
+      },
+      visible: {
+        opacity: 1,
+        transition: {
+          delay: 0.5,
+          duration: 0.5,
+        },
+      },
+    };
+
+    const detailSectionAnimationVariant = {
+      hidden: {
+        opacity: 0,
+        transform: "translateX(-100px)",
+      },
+      visible: {
+        opacity: 1,
+        transform: "translateX(0px)",
+        transition: {
+          delay: 0.5,
+          duration: 0.5,
+          staggerChildren: 2,
+        },
+      },
+    };
+
+    const detailChildrenAnimationVariant = {
+      hidden: {
+        opacity: 0,
+        transform: "translateX(-100px)",
+      },
+      visible: {
+        opacity: 1,
+        transform: "translateX(0px)",
+        transition: {
+          staggerChildren: 0.6,
+        },
+      },
+    };
+
     return (
       <section className="introduction-section">
-        <div className="display">
+        <motion.div
+          className="display"
+          variants={displaySectionAnimationVariant}
+          initial="hidden"
+          animate="visible"
+        >
           <HorizontalMoveImageViewer
             ref={ref}
             images={props.images}
             onImageChange={props.onImageChange}
             showArrow={true}
           />
-        </div>
-        <div className="details">
-          {/* <div className="color-options">
-          {ProductData.colorOptions.map((color, idx) => {
-            return (
-              <div
-                key={idx}
-                className={
-                  "color-option" + (selectedColor === color ? " selected" : "")
-                }
-                style={{ backgroundColor: color }}
-                onClick={() => setSelectedColor(color)}
-              ></div>
-            );
-          })}
-        </div> */}
-          <div className="info-container">
-            <h2 className="title">{props.title}</h2>
-            <h3 className="sub-title">{props.subTitle}</h3>
-            <StringToUppercasedFirstLetterParagraphElement
-              str={props.description}
-              elementClassName="paragraph desc"
-              spanClassName="first-letter"
-            />
-          </div>
-          <div className="form-button-container">
+        </motion.div>
+        <motion.div
+          className="details"
+          variants={detailSectionAnimationVariant}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.div
+            className="info-container"
+            variants={detailChildrenAnimationVariant}
+          >
+            <motion.h2
+              className="title"
+              variants={detailChildrenAnimationVariant}
+            >
+              {props.title}
+            </motion.h2>
+            <motion.h3
+              className="sub-title"
+              variants={detailChildrenAnimationVariant}
+            >
+              {props.subTitle}
+            </motion.h3>
+            <motion.div variants={detailChildrenAnimationVariant}>
+              <StringToUppercasedFirstLetterParagraphElement
+                str={props.description}
+                elementClassName="paragraph desc"
+                spanClassName="first-letter"
+              />
+            </motion.div>
+          </motion.div>
+          <motion.div
+            className="form-button-container"
+            variants={detailChildrenAnimationVariant}
+          >
             <Link to={props.exploreUrl as string}>
               <FormButton>Explore More</FormButton>{" "}
             </Link>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
         {props.children}
       </section>
     );
