5ca780b18f7ce7e94c125f840f30926ea9c6c486
rabbitblood <qz154772610@gmail.com>
2024-06-09 13:16:34 -0700

    shorten meta desc

--- src/data/SEOData.ts
+++ src/data/SEOData.ts
@@ -1,6 +1,6 @@
 const generalTitleMetaData = "Vé Studio - Handmade Leather Goods in Vancouver";
 const generalDescriptionMetaData =
-  "Explore Vé Studio's exclusive range of handmade leather goods, crafted with elegance and precision in Vancouver, BC. From stylish accessories like chokers and bracelets to personalized gift options, discover quality and craftsmanship in every piece.";
+  "Explore Vé Studio's exclusive range of handmade leather goods, crafted with elegance and precision in Vancouver, BC.";
 const generalKeywordsMetaData =
   "Vé, Vé Studio, Ve, Handmade Leather Goods,Vancouver, BC, Canada , Handmade Leather Products, Handmade, Custom Leather Goods, Leather, Products, Canada, BC, Vancouver, Elegant, Accessories, Choker, Bracelet, Earrings, Necklace, Ring, Gift Set, Gift Wrap, ve studio canada, vestudio vancouver, ve studio richmond";
 const featureImageUrl =
