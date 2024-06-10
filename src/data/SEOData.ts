a6fe37f6e654c82df25d263c1d0375e61c852325
rabbitblood <qz154772610@gmail.com>
2024-06-09 18:52:28 -0700

    reduce some keywords

--- src/data/SEOData.ts
+++ src/data/SEOData.ts
@@ -2,7 +2,7 @@ const generalTitleMetaData = "Vé Studio - Handmade Leather Goods in Vancouver";
 const generalDescriptionMetaData =
   "Explore Vé Studio's exclusive range of handmade leather goods, crafted with elegance and precision in Vancouver, BC.";
 const generalKeywordsMetaData =
-  "Vé, Vé Studio, Ve, Handmade Leather Goods,Vancouver, BC, Canada , Handmade Leather Products, Handmade, Custom Leather Goods, Leather, Products, Canada, BC, Vancouver, Elegant, Accessories, Choker, Bracelet, Earrings, Necklace, Ring, Gift Set, Gift Wrap, ve studio canada, vestudio vancouver, ve studio richmond";
+  "Vé, Vé Studio, Ve, Handmade Leather Goods,Vancouver, BC, Canada , Handmade Leather Products, Handmade, Custom Leather Goods, Leather, Products,  Elegant, Accessories, Choker, Bracelet, Earrings, Necklace, Ring, Gift Set, Gift Wrap, ve studio canada, vestudio vancouver, ve studio richmond";
 const featureImageUrl =
   "https://cdn.builder.io/api/v1/image/assets%2F37ce5e0c9af24b75813430f31b21364f%2Fc7925b80c5b04451a290c7eaf988c0bb";
 
